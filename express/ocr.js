6260c678d5f8b965c0ad8b685fe083c83b9fd550
hashan malinda <85447795+hashan789@users.noreply.github.com>
2022-08-03 16:34:14 +0530

    Update ocr.js

--- express/ocr.js
+++ express/ocr.js
@@ -24,6 +24,7 @@ const upload = multer({storage: storage}).single("avatar");
 
 //app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
 app.engine('ejs', require('ejs').__express);
 
 app.use('/public', express.static('public'));
